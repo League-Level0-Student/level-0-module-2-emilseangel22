4a5e51400f00de848c2d6368d912bd3870fd8a20
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-21 13:26:11 -0700

    DiceRoll, IntroToRandom, RockPaperScissors

--- src/random/RockPaperScissors.java
+++ src/random/RockPaperScissors.java
@@ -6,6 +6,7 @@ package random;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -81,8 +82,9 @@ public class RockPaperScissors extends JPanel implements ActionListener{
         //2. Run the program 4 times. Does the computer always choose the same thing?
 
         //3. Change the value of opponentSelection to be a random number between 0 and 2;
-        int opponentSelection = 0;
         
+        Random Factory = new Random();
+        int opponentSelection = Factory.nextInt(3)+0;
         //4. Run the program again. Is the result different?
  
         int selection = 0;
