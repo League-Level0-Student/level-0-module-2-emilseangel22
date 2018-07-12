0e0e3b8cecb954f1a9572e3ccfcca9523f615dc7
emilseangel22 <37815806+emilseangel22@users.noreply.github.com>
2018-07-12 13:06:15 -0700

    RollCoaster AnimalFram ip

---
+++ src/random/RollerCoaster.java
@@ -0,0 +1,14 @@
+package random;
+
+import javax.swing.JOptionPane;
+
+public class RollerCoaster {public static void main(String[] args) {
+	String answer=JOptionPane.showInputDialog(null, "How tall are you?");
+	int height = Integer.parseInt(answer);
+	if (height>=4) {
+		JOptionPane.showMessageDialog(null, "You are able to go on the ride.");
+	}.
+	else(height<=4){
+}
+}
+}
