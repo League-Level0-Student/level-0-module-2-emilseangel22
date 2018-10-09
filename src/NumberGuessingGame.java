417f95d86dd658b19d793ac399b5460c0107bf77
337296 <337296@0338S-S-R0B6ZHR.sdcs.local>
2018-10-09 14:26:00 -0700

    MolesinHoles, NumberGussing ip

---
+++ src/NumberGuessingGame.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class NumberGuessingGame {
+public static void main(String[] args) {
+	int choice = 1;
+	Random F = new Random();
+	JOptionPane.showInputDialog(null,"What number am i guessing about");
+	if 
+}
+}
+
+
